819ffb68b9780d35f0c8cd0cbdf494bb09e6e392
dotcmsbuild <dotcmsbuild@dotcms.com>
2020-09-23 08:02:47 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const core = require('@actions/core');
+const io = require("@actions/io");
 const wait = require('./wait');
-
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const path = core.getInput('path', { required: true });
+    await io.rmRF(path);
+    core.debug(path); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
+ 
--- index.test.js
+++ index.test.js
@@ -1,23 +1,14 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+const fs = require("fs");
+const io = require("@actions/io");
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+test('creates directory successfully', async () => {
+  await io.mkdirP("path/to/make");
+  const path = fs.existsSync("path/to/make");
+  expect(path).toBe(true);
 });
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+test('removes directory successfully', async () => {
+    await io.rmRF("path");
+    const path = fs.existsSync("path/to/make");
+    expect(path).toBe(false);
 })
